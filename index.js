1c15155fdad7f29d1cea531f084255ad40030e5f
Leonardo Valeriano <leonardo.valeriano.2000@gmail.com>
2022-08-28 19:06:41 -0500

    Form Handling

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const { create } = require('express-handlebars')
 const handlers = require('./lib/handlers')
 const weatherMiddleware = require('./lib/middleware/weather')
+const bodyParser = require('body-parser')
+const multiparty = require('multiparty')
 
 const hbs = create({
     defaultLayout: 'main',
@@ -24,6 +26,8 @@ app.set('view cache', true)
 
 app.use(express.static(__dirname + '/public'))
 app.use(weatherMiddleware)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 3000
 
@@ -32,6 +36,45 @@ app.get('/', handlers.home)
 app.get('/about', handlers.about)
 
 app.get('/section-test', handlers.sectionTest)
+
+// handlers for browser-based form submission
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+
+// handlers for fetch/JSON form submission
+app.get('/newsletter', handlers.newsletter)
+app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
+
+// vacation photo contest
+app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
+app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax)
+app.post('/contest/vacation-photo/:year/:month', (req, res) => {
+    const form = new multiparty.Form()
+    form.parse(req, (err, fields, files) => {
+        if (err)
+            return handlers.vacationPhotoContestProcessError(
+                req,
+                res,
+                err.message
+            )
+        console.log('got fields: ', fields)
+        console.log('and files: ', files)
+        handlers.vacationPhotoContestProcess(req, res, fields, files)
+    })
+})
+app.get(
+    '/contest/vacation-photo-thank-you',
+    handlers.vacationPhotoContestProcessThankYou
+)
+app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
+    const form = new multiparty.Form()
+    form.parse(req, (err, fields, files) => {
+        if (err)
+            return handlers.api.vacationPhotoContestError(req, res, err.message)
+        handlers.api.vacationPhotoContest(req, res, fields, files)
+    })
+})
 
 app.use(handlers.notFound)
 
