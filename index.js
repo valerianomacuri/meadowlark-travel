eee60e3ee1b68889a350cc8b3a46c21c64bf0950
Leonardo Valeriano <leonardo.valeriano.2000@gmail.com>
2022-08-30 12:43:15 -0500

    Cookies and Sessions

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const { create } = require('express-handlebars')
 const handlers = require('./lib/handlers')
 const weatherMiddleware = require('./lib/middleware/weather')
 const bodyParser = require('body-parser')
-const multiparty = require('multiparty')
+const cookieParser = require('cookie-parser')
+const expressSession = require('express-session')
+const credentials = require('./.credentials.development')
+const flashMiddleware = require('./lib/middleware/flash')
+const { check } = require('express-validator')
+const validateNewsletterSignup = require('./lib/middleware/validateNewsletterSignup')
+require('dotenv').config()
 
+const port = process.env.PORT || 3000
+
+// handlebars config
 const hbs = create({
     defaultLayout: 'main',
     helpers: {
@@ -20,61 +29,49 @@ const hbs = create({
 
 const app = express()
 
+//* Templating with Handlebars
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
-app.set('view cache', true)
 
-app.use(express.static(__dirname + '/public'))
-app.use(weatherMiddleware)
+//* Form data
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-const port = process.env.PORT || 3000
+//* Cookies and Sessions
+app.use(cookieParser(credentials.cookieSecret))
+app.use(
+    expressSession({
+        resave: false,
+        saveUninitialized: false,
+        secret: credentials.cookieSecret,
+    })
+)
+
+//* Serve static files
+app.use(express.static(__dirname + '/public'))
+
+app.use(weatherMiddleware)
+app.use(flashMiddleware)
 
 app.get('/', handlers.home)
 
 app.get('/about', handlers.about)
 
-app.get('/section-test', handlers.sectionTest)
-
 // handlers for browser-based form submission
 app.get('/newsletter-signup', handlers.newsletterSignup)
-app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
-app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
-
-// handlers for fetch/JSON form submission
-app.get('/newsletter', handlers.newsletter)
-app.post('/api/newsletter-signup', handlers.api.newsletterSignup)
-
-// vacation photo contest
-app.get('/contest/vacation-photo', handlers.vacationPhotoContest)
-app.get('/contest/vacation-photo-ajax', handlers.vacationPhotoContestAjax)
-app.post('/contest/vacation-photo/:year/:month', (req, res) => {
-    const form = new multiparty.Form()
-    form.parse(req, (err, fields, files) => {
-        if (err)
-            return handlers.vacationPhotoContestProcessError(
-                req,
-                res,
-                err.message
-            )
-        console.log('got fields: ', fields)
-        console.log('and files: ', files)
-        handlers.vacationPhotoContestProcess(req, res, fields, files)
-    })
-})
-app.get(
-    '/contest/vacation-photo-thank-you',
-    handlers.vacationPhotoContestProcessThankYou
+app.post(
+    '/newsletter-signup/process',
+    [
+        check(
+            'email',
+            'The email address you entered was not valid.'
+        ).isEmail(),
+        validateNewsletterSignup,
+    ],
+    handlers.newsletterSignupProcess
 )
-app.post('/api/vacation-photo-contest/:year/:month', (req, res) => {
-    const form = new multiparty.Form()
-    form.parse(req, (err, fields, files) => {
-        if (err)
-            return handlers.api.vacationPhotoContestError(req, res, err.message)
-        handlers.api.vacationPhotoContest(req, res, fields, files)
-    })
-})
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+app.get('/newsletter-archive', handlers.newsletterSignupThankYou)
 
 app.use(handlers.notFound)
 
